27df58e008d60a7ad842c11d6a6c0127eb0b89cf
joyAct <joyleeact@gmail.com>
2020-12-14 16:36:00 +0900

    SVG Icon Components

---
+++ src/components/icons.js
@@ -0,0 +1,50 @@
+export const MenuIcon = (props) => {
+  return (
+    <svg
+      {...props}
+      fill="currentColor"
+      viewBox="0 0 20 20"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fillRule="evenodd"
+        d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+        clipRule="evenodd"
+      />
+    </svg>
+  );
+};
+
+export const ArrowLeftIcon = (props) => {
+  return (
+    <svg
+      {...props}
+      fill="currentColor"
+      viewBox="0 0 20 20"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fillRule="evenodd"
+        d="M9.707 16.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l4.293 4.293a1 1 0 010 1.414z"
+        clipRule="evenodd"
+      />
+    </svg>
+  );
+};
+
+export const SearchIcon = (props) => {
+  return (
+    <svg
+      {...props}
+      fill="currentColor"
+      viewBox="0 0 20 20"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fillRule="evenodd"
+        d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+        clipRule="evenodd"
+      />
+    </svg>
+  );
+};
--- src/components/main/nav-btn.js
+++ src/components/main/nav-btn.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import { MenuIcon } from '../icons';
 
 const Container = styled.div`
   padding: 15px 20px;
 `;
 
-const MenuIcon = styled.svg`
+const MenuIconStyle = styled(MenuIcon)`
   width: 25px;
   color: #333;
 `;
@@ -15,18 +16,7 @@ function NavBtn() {
   return (
     <Container>
       <Link to="/login">
-        <MenuIcon
-          className="w-6 h-6"
-          fill="currentColor"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fillRule="evenodd"
-            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-            clipRule="evenodd"
-          />
-        </MenuIcon>
+        <MenuIconStyle />
       </Link>
     </Container>
   );
