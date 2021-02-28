9a9a049d4c5edc491f26e20cd6bf5962dffe8dd6
Joy Lee <joylee.dev@gmail.com>
2021-02-28 19:05:27 +0900

    Redirect (fix error)

--- src/components/Route.js
+++ src/components/Route.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Route as BaseRoute, useHistory } from 'react-router-dom';
+import { Route as BaseRoute, useHistory, Redirect } from 'react-router-dom';
 import { useUserContext } from '../contexts/UserContext';
 
 function Route({ auth, ...props }) {
   const { user } = useUserContext();
-  const history = useHistory();
+  // const history = useHistory();
 
   // 로그인 x : 로그인페이지로
-  if (auth && user === null) {
-    history.replace('./login');
+  if (auth && !user) {
+    return <Redirect to="./login" />;
   }
 
   // 로그인 o : 해당 링크로
